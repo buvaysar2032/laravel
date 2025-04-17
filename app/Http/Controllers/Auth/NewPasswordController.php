81f95df8e9c03ae28992725aebbc30feb00909c6
Buvaysar <buvaysar2032@gmail.com>
2025-04-18 00:06:18 +0300

    feat(api): документация API через Swagger

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\OpenApi\Attributes\RequestFormData;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -11,6 +12,11 @@ use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Illuminate\Validation\ValidationException;
+use OpenApi\Attributes\Items;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Post;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
 
 class NewPasswordController extends Controller
 {
@@ -19,6 +25,55 @@ class NewPasswordController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
+    #[Post(
+        path: '/api/reset-password',
+        operationId: 'resetPassword',
+        description: 'Позволяет пользователю сбросить пароль по предоставленному токену.',
+        summary: 'Сброс пароля пользователя по токену',
+        tags: ['Auth']
+    )]
+    #[RequestFormData(
+        requiredProps: ['token', 'email', 'password', 'password_confirmation'],
+        properties: [
+            new Property(property: 'token', description: '', type: 'string'),
+            new Property(property: 'email', description: '', type: 'string'),
+            new Property(property: 'password', description: '', type: 'string'),
+            new Property(property: 'password_confirmation', description: '', type: 'string'),
+        ]
+    )]
+    #[Response(
+        response: 200,
+        description: 'Пароль был успешно сброшен.',
+        content: new JsonContent(
+            properties: [
+                new Property(property: 'status', type: 'string', example: 'Ваш пароль был сброшен.'),
+            ]
+        )
+    )]
+    #[Response(
+        response: 422,
+        description: 'Ошибка: Необрабатываемое содержимое',
+        content: new JsonContent(
+            properties: [
+                new Property(property: "message", type: "string", example: "Значение поля email адрес должно быть действительным электронным адресом."),
+                new Property(
+                    property: "errors",
+                    properties: [
+                        new Property(
+                            property: "email",
+                            type: "array",
+                            items: new Items(type: "string", example: "Значение поля email адрес должно быть действительным электронным адресом.")
+                        ),new Property(
+                            property: "password",
+                            type: "array",
+                            items: new Items(type: "string", example: "Количество символов в поле пароль должно быть не меньше 8.")
+                        ),
+                    ],
+                    type: "object"
+                )
+            ]
+        )
+    )]
     public function store(Request $request): JsonResponse
     {
         $request->validate([
