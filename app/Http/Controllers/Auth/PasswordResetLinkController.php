79222a481ebd04714c30852aab78acce77ec4ab0
Buvaysar <buvaysar2032@gmail.com>
2025-04-17 18:41:41 +0300

    fix(AppServiceProvider): boot

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\OpenApi\Attributes\RequestFormData;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
+use OpenApi\Attributes\Items;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Post;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
 
 class PasswordResetLinkController extends Controller
 {
@@ -15,6 +21,48 @@ class PasswordResetLinkController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
+    #[Post(
+        path: '/api/forgot-password',
+        operationId: 'sendPasswordResetLink',
+        description: 'Отправка письма со ссылкой на сброс пароля пользователю по email.',
+        summary: 'Запрос на сброс пароля',
+        tags: ['Auth']
+    )]
+    #[RequestFormData(
+        requiredProps: ['email'],
+        properties: [
+            new Property(property: 'email', description: 'Email', type: 'string'),
+        ]
+    )]
+    #[Response(
+        response: 200,
+        description: 'Ссылка для сброса пароля отправлена',
+        content: new JsonContent(
+            properties: [
+                new Property(property: 'status', type: 'string', example: 'Ссылка на сброс пароля была отправлена.'),
+            ]
+        )
+    )]
+    #[Response(
+        response: 422,
+        description: 'Ошибка: Необработанный контент',
+        content: new JsonContent(
+            properties: [
+                new Property(property: "message", type: "string", example: "Значение поля email адрес должно быть действительным электронным адресом."),
+                new Property(
+                    property: "errors",
+                    properties: [
+                        new Property(
+                            property: "email",
+                            type: "array",
+                            items: new Items(type: "string", example: "Не удалось найти пользователя с указанным электронным адресом.")
+                        ),
+                    ],
+                    type: "object"
+                )
+            ]
+        )
+    )]
     public function store(Request $request): JsonResponse
     {
         $request->validate([
