7b5adbea306ac5ac34311a8fdfa51d88401d8419
Buvaysar <buvaysar2032@gmail.com>
2025-04-18 11:47:24 +0300

    feat: Moonshine

---
+++ app/Models/Text.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Text extends Model
+{
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+}
---
+++ app/MoonShine/Resources/TextResource.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Text;
+
+use MoonShine\CKEditor\Fields\CKEditor;
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Fields\Date;
+use MoonShine\UI\Fields\ID;
+use MoonShine\Contracts\UI\FieldContract;
+use MoonShine\Contracts\UI\ComponentContract;
+
+/**
+ * @extends ModelResource<Text>
+ */
+class TextResource extends ModelResource
+{
+    protected string $model = Text::class;
+
+    protected string $title = 'Тексты';
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function indexFields(): iterable
+    {
+        return [
+            ID::make()->sortable(),
+            \MoonShine\UI\Fields\Text::make('Ключ', 'key'),
+            CKEditor::make('Значение', 'value'),
+            Date::make('Дата создания', 'created_at')->format('d.m.Y H:i')->sortable(),
+            Date::make('Дата изменения', 'updated_at')->format('d.m.Y H:i')->sortable()
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract|FieldContract>
+     */
+    protected function formFields(): iterable
+    {
+        return [
+            Box::make([
+                ID::make(),
+                \MoonShine\UI\Fields\Text::make('Ключ', 'key'),
+                CKEditor::make('Значение', 'value'),
+            ])
+        ];
+    }
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function detailFields(): iterable
+    {
+        return $this->indexFields();
+    }
+
+    /**
+     * @param Text $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    protected function rules(mixed $item): array
+    {
+        return [
+            'key' => ['required', 'string'],
+            'value' => ['required', 'string', 'min:2']
+        ];
+    }
+}
