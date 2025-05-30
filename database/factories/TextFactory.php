37ce1916f1d754014792a2e4fabbb4f4f934cf32
Buvaysar <buvaysar2032@gmail.com>
2025-05-30 16:27:34 +0300

    feat: textTest

---
+++ database/factories/TextFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Text>
+ */
+class TextFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'key' => fake()->biasedNumberBetween(1, 10),
+            'value' => fake()->paragraph()
+        ];
+    }
+}
